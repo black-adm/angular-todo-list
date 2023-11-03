46888fd4f713f56fdeca2125802a8c26c6d9cbc7
black-adm <mattheuscontato17@gmail.com>
2023-11-03 08:57:30 -0300

    testing functions in title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public todos: any[] = [];
-  public title: String = 'Minhas tarefas';
+  public title: String = 'Minha lista de tarefas';
 
   constructor() {
     this.todos.push('Estudar angular');
     this.todos.push('Ir para academia');
     this.todos.push('Fazer a Alice dormir');
+  }
+
+  changeTitle() {
+    this.title = 'Meus objetivos diários'
   }
 }
